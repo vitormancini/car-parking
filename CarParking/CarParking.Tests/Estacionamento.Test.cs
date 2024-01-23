37a4b8c3b2ea27d1fd42dfe30e34588864e12a84
Vitor Mancini <vitor.mancini21@hotmail.com>
2024-01-22 21:39:17 -0300

    Final

--- CarParking/CarParking.Tests/Estacionamento.Test.cs
+++ CarParking/CarParking.Tests/Estacionamento.Test.cs
@@ -14,11 +14,20 @@
 */
 
 using CarParking.Models;
+using Xunit.Abstractions;
 
 namespace CarParking.Tests
 {
     public class EstacionamentoTest
     {
+        public ITestOutputHelper _saidaConsole; // Permite que escrevemos mensagens no console
+
+        public EstacionamentoTest(ITestOutputHelper saidaConsole)
+        {
+            _saidaConsole = saidaConsole;
+            _saidaConsole.WriteLine("Testando console...");
+        }
+
         [Fact]
         public void ValidaFaturamento()
         {
@@ -88,42 +97,6 @@ namespace CarParking.Tests
             var consulta = estacionamento.LocalizaVeiculo(placa); // Método ainda não existente. Teste criado primeiro
 
             Assert.Equal(placa, consulta.Placa);
-        }
-
-        // Escrita do teste antes da implementação do método (TDD)
-        [Fact]
-        public void ValidaAlterarDadosVeiculo()
-        {
-            Estacionamento estacionamento = new Estacionamento();
-
-            Veiculo veiculo = new Veiculo();
-            veiculo.Placa = "AAA-1111";
-            veiculo.Proprietario = "Vitor";
-            veiculo.Cor = "Preto";
-            estacionamento.RegistrarEntrada(veiculo);
-
-            Veiculo veiculoAlterado = new Veiculo();
-            veiculoAlterado.Placa = "AAA-1111"; 
-            veiculoAlterado.Proprietario = "Vitor";
-            veiculoAlterado.Cor = "Azul"; // Alteração
-
-            Veiculo alterado = estacionamento.AlterarDadosVeiculo(veiculoAlterado); // Método ainda não existente. Teste criado primeiro
-
-            Assert.Equal(alterado.Cor, veiculoAlterado.Cor);
-        }
-
-        // Contains
-        [Fact]
-        public void ValidaFichaVeiculo()
-        {
-            Veiculo veiculo = new Veiculo();
-            veiculo.Placa = "AAA-1111";
-            veiculo.Proprietario = "Vitor";
-            veiculo.Cor = "Preto";
-
-            string ficha = veiculo.ToString();
-
-            Assert.Contains("Ficha do veiculo", ficha);
         }
     }
 }
---
+++ CarParking/CarParking.Tests/Veiculo.Test.cs
@@ -0,0 +1,79 @@
+﻿using CarParking.Models;
+using Xunit.Abstractions;
+
+namespace CarParking.Tests
+{
+    public class VeiculoTest
+    {
+
+        public ITestOutputHelper _saidaConsole; // Permite que escrevemos mensagens no console
+
+        public VeiculoTest(ITestOutputHelper saidaConsole)
+        {
+            _saidaConsole = saidaConsole;
+            _saidaConsole.WriteLine("Testando console...");
+        }
+
+        // Escrita do teste antes da implementação do método (TDD)
+        [Fact]
+        public void ValidaAlterarDadosVeiculo()
+        {
+            Estacionamento estacionamento = new Estacionamento();
+
+            Veiculo veiculo = new Veiculo();
+            veiculo.Placa = "AAA-1111";
+            veiculo.Proprietario = "Vitor";
+            veiculo.Cor = "Preto";
+            estacionamento.RegistrarEntrada(veiculo);
+
+            Veiculo veiculoAlterado = new Veiculo();
+            veiculoAlterado.Placa = "AAA-1111";
+            veiculoAlterado.Proprietario = "Vitor";
+            veiculoAlterado.Cor = "Azul"; // Alteração
+
+            Veiculo alterado = estacionamento.AlterarDadosVeiculo(veiculoAlterado); // Método ainda não existente. Teste criado primeiro
+
+            Assert.Equal(alterado.Cor, veiculoAlterado.Cor);
+        }
+
+        // Contains
+        [Fact]
+        public void ValidaFichaVeiculo()
+        {
+            Veiculo veiculo = new Veiculo();
+            veiculo.Placa = "AAA-1111";
+            veiculo.Proprietario = "Vitor";
+            veiculo.Cor = "Preto";
+
+            string ficha = veiculo.ToString();
+
+            Assert.Contains("Ficha do veiculo", ficha);
+        }
+
+        // Teste de exceção
+        [Fact]
+        public void ValidaExceçãoNomeProprietario()
+        {
+            // Arrange
+            string nomeProprietario = "Vit3r56";
+
+            Veiculo veiculo = new Veiculo();
+
+            // Assert                                     // Act
+            Assert.Throws<System.FormatException>( () => veiculo.Proprietario = nomeProprietario);
+        }
+
+        // Teste de exceção
+        [Fact]
+        public void ValidaExceçãoPlacaVeiculo()
+        {
+            // Arrange
+            string placaVeiculo = "aaa3412";
+
+            Veiculo veiculo = new Veiculo();
+
+            // Assert                                     // Act
+            Assert.Throws<System.FormatException>(() => veiculo.Placa = placaVeiculo);
+        }
+    }
+}
